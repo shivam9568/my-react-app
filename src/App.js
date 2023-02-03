f5eab4e742aa573617c39f36d52ef330ee5cae27
shivam9568 <120097408+shivam9568@users.noreply.github.com>
2023-02-03 15:42:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar';
+import About from './Components/about';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes, Route, BrowserRouter
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
   return (
-   <Navbar/>
+ <>
+  <BrowserRouter>
+<Navbar/>
 
-  );
-}
+ <Routes>
+  
+  <Route path="/" element={<div> hello</div>}/>
+  <Route path="/about" element={<About/>}>
+    
+  </Route>
 
-export default App;
+
+
+ </Routes>
+ </BrowserRouter>
+
+ </>
+      );
+      }
+
+      export default App;
+  
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -14,7 +15,7 @@ export default function Navbar() {
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <Link className="nav-link" to="/about">About</Link>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
