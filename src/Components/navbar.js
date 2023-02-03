3dc71df40db1eab6c34a5d9acfe953cbd9256465
shivam9568 <120097408+shivam9568@users.noreply.github.com>
2023-02-03 16:53:21 +0530

    Update navbar.js

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={"/about"}>About</Link>
+          <a className="nav-link" href={"/about"}>About</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
